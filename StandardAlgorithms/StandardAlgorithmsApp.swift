0eb083c4294c4325361ba15ad1a93c6634f8a3f2
AndrewG179 <rich.feynman.f@gmail.com>
2024-11-05 10:05:47 +0000

    Initial Commit

---
+++ StandardAlgorithms/StandardAlgorithmsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StandardAlgorithmsApp.swift
+//  StandardAlgorithms
+//
+//  Created by Goryainov, Andrey (SPH) on 05/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct StandardAlgorithmsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
