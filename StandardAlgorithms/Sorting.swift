097ec91f469693933b631e680a645d19ee43bbb2
Bromum <goryainov2487@gmail.com>
2024-11-18 21:12:31 +0000

    Finished Standard Algorithms in swift (Eton)

--- StandardAlgorithms/Sorting.swift
+++ StandardAlgorithms/Sorting.swift
@@ -10,35 +10,109 @@ import Foundation
 class Sorting {
     func mergeSort(_ x: [Int]) -> [Int] {
         let n = x.count
-        if (n == 1) {return x}
-        if (n == 0) {return []}
+        if n == 1 { return x }
+        if n == 0 { return [] }
+        
         var sorted: [Int] = []
         let m = n / 2
         let a = mergeSort(Array(x.prefix(m)))
         let b = mergeSort(Array(x.suffix(from: m)))
         
         var i = 0, j = 0
-        while (i < a.count && j < b.count) {
-            while (i < a.count && j < b.count && a[i] <= b[j]) {
-                sorted.append(a[i]);
-                i += 1;
-            }
-            while (i < a.count && j < b.count && a[i] > b[j]) {
-                sorted.append(b[j]);
-                j += 1;
+        while i < a.count && j < b.count {
+            if a[i] <= b[j] {
+                sorted.append(a[i])
+                i += 1
+            } else {
+                sorted.append(b[j])
+                j += 1
             }
         }
-        while (i < a.count) {
+        while i < a.count {
             sorted.append(a[i])
-            i += 1;
+            i += 1
         }
-        while (j < b.count) {
+        while j < b.count {
             sorted.append(b[j])
-            j += 1;
+            j += 1
         }
         return sorted
     }
-    func binarySearch(_ m: [Int], _ x: Int) -> [Int] {
+    
+    func binarySearch(_ x: [Int], _ toFind: Int) -> Int {
+        var l = -1
+        var r = x.count
+        while r - l > 1 {
+            let m = l + (r - l) / 2
+            if x[m] < toFind {
+                l = m
+            } else {
+                r = m
+            }
+        }
+        return r
     }
     
+    func linearSearch(_ x: [Int], _ value: Int) -> Int? {
+        for i in 0..<x.count {
+            if x[i] == value {
+                return i
+            }
+        }
+        return nil
+    }
     
+    func quickSort(_ x: [Int]) -> [Int] {
+        let n = x.count
+        if n <= 1 { return x }
+        
+        let pivot = x[n / 2]
+        var less: [Int] = []
+        var equal: [Int] = []
+        var greater: [Int] = []
+        
+        for i in 0..<n {
+            if x[i] < pivot {
+                less.append(x[i])
+            } else if x[i] == pivot {
+                equal.append(x[i])
+            } else {
+                greater.append(x[i])
+            }
+        }
+        
+        let sortedLess = quickSort(less)
+        let sortedGreater = quickSort(greater)
+        return sortedLess + equal + sortedGreater
+    }
+    
+    func bubbleSort(_ x: [Int]) -> [Int] {
+        var x = x
+        let n = x.count
+        for i in 0..<n {
+            for j in 0..<(n - i - 1) {
+                if x[j] > x[j + 1] {
+                    let temp = x[j]
+                    x[j] = x[j + 1]
+                    x[j + 1] = temp
+                }
+            }
+        }
+        return x
+    }
+    
+    func insertionSort(_ x: [Int]) -> [Int] {
+        var x = x
+        let n = x.count
+        for i in 1..<n {
+            var j = i
+            while j > 0 && x[j] < x[j - 1] {
+                let temp = x[j]
+                x[j] = x[j - 1]
+                x[j - 1] = temp
+                j -= 1
+            }
+        }
+        return x
+    }
+}
--- StandardAlgorithmsTests/SortingTest.swift
+++ StandardAlgorithmsTests/SortingTest.swift
@@ -25,8 +25,9 @@ final class SortingTest: XCTestCase {
             let result = sorting.mergeSort(testCase.a)
 
             XCTAssertEqual(result, testCase.expectedA)
-        }   
+        }
     }
+    
     func testAllBubbleSortIntAscending () {
         //arrange --setup
         let sorting = Sorting()
@@ -40,9 +41,85 @@ final class SortingTest: XCTestCase {
         //act -- run
         //assert -- check
         for testCase in testCases {
-            let result = sorting.mergeSort(testCase.a) //change to bubble
+            let result = sorting.bubbleSort(testCase.a)
 
             XCTAssertEqual(result, testCase.expectedA)
+        }
+    }
+    
+    func testAllQuickSortIntAscending () {
+        //arrange --setup
+        let sorting = Sorting()
+        let testCases = [
+                            (a: [3, 1, 4, 5, 2], expectedA: [1, 2, 3, 4, 5]),
+                            (a: [7, 3, 1, 4, 9, 5, 8, 2, 6], expectedA: [1, 2, 3, 4, 5, 6, 7, 8, 9]),
+                            (a: [], expectedA: []),
+                            (a: [2, 1, 2, 1, 5, 3, 4], expectedA: [1, 1, 2, 2, 3, 4, 5]),
+                            (a: [7, -1, 5, 0, -2], expectedA: [-2, -1, 0, 5, 7]),
+        ]
+        //act -- run
+        //assert -- check
+        for testCase in testCases {
+            let result = sorting.quickSort(testCase.a)
+
+            XCTAssertEqual(result, testCase.expectedA)
+        }
+    }
+    
+    func testAllInsertionSortIntAscending () {
+        //arrange --setup
+        let sorting = Sorting()
+        let testCases = [
+                            (a: [3, 1, 4, 5, 2], expectedA: [1, 2, 3, 4, 5]),
+                            (a: [7, 3, 1, 4, 9, 5, 8, 2, 6], expectedA: [1, 2, 3, 4, 5, 6, 7, 8, 9]),
+                            (a: [], expectedA: []),
+                            (a: [2, 1, 2, 1, 5, 3, 4], expectedA: [1, 1, 2, 2, 3, 4, 5]),
+                            (a: [7, -1, 5, 0, -2], expectedA: [-2, -1, 0, 5, 7]),
+        ]
+        //act -- run
+        //assert -- check
+        for testCase in testCases {
+            let result = sorting.insertionSort(testCase.a)
+
+            XCTAssertEqual(result, testCase.expectedA)
+        }
+    }
+    
+    func testBinarySearch () {
+        //arrange --setup
+        let sorting = Sorting()
+        let testCases = [
+                            (x: [1, 2, 3, 4, 5], toFind: 3, expectedIndex: 2),
+                            (x: [1, 3, 5, 7, 9], toFind: 7, expectedIndex: 3),
+                            (x: [1, 2, 3, 5, 7, 9], toFind: 2, expectedIndex: 1),
+                            (x: [1, 2], toFind: 1, expectedIndex: 0),
+                            (x: [1], toFind: 1, expectedIndex: 0),
+        ]
+        //act -- run
+        //assert -- check
+        for testCase in testCases {
+            let result = sorting.binarySearch(testCase.x, testCase.toFind)
+
+            XCTAssertEqual(result, testCase.expectedIndex)
+        }
+    }
+    
+    func testLinearSearch () {
+        //arrange --setup
+        let sorting = Sorting()
+        let testCases = [
+                            (x: [1, 2, 3, 4, 5], toFind: 3, expectedIndex: 2),
+                            (x: [1, 3, 5, 7, 9], toFind: 7, expectedIndex: 3),
+                            (x: [1, 2, 3, 5, 7, 9], toFind: 2, expectedIndex: 1),
+                            (x: [1, 2], toFind: 1, expectedIndex: 0),
+                            (x: [1], toFind: 1, expectedIndex: 0),
+        ]
+        //act -- run
+        //assert -- check
+        for testCase in testCases {
+            let result = sorting.linearSearch(testCase.x, testCase.toFind)
+
+            XCTAssertEqual(result, testCase.expectedIndex)
         }
     }
 }
